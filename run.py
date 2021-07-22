8e0f215e9c4a867d46d01ae4470d69ef2d9d6681
Kevin Arne <kevin.arne@gmail.com>
2021-07-22 12:41:13 -0700

    Having trouble pulling air quality info from the website. It appears to be loaded via JAVASCRIPT, but polling with Selenium hasn't worked.

--- run.py
+++ run.py
@@ -2,12 +2,17 @@ import requests
 from decouple import config
 from plyer import notification
 import smtplib, ssl
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+driver = webdriver.Chrome()
 
 port = 465
 
 if __name__ == "__main__":
     # Request two days of data (today and tomorrow) from openweather at the location used in environmental variables.
-    response = requests.get('https://api.openweathermap.org/data/2.5/onecall'+
+    weather_response = requests.get(
+        'https://api.openweathermap.org/data/2.5/onecall'+
         f"?lat={config('LATITUDE')}" +
         f"&lon={config('LONGITUDE')}" +
         "&units=imperial" +
@@ -15,13 +20,25 @@ if __name__ == "__main__":
         f"&APPID={config('OPEN_WEATHER_API')}"
         )
     # Access the second day temperatures and the associated maximum
-    maxtemp = response.json()['daily'][1]['temp']['max']
+    maxtemp = weather_response.json()['daily'][1]['temp']['max']
 
     notification.notify(
         title="Weather Alert",
         message=f"Tomorrow's max temp is {maxtemp}F",
         timeout=10
     )
+
+    driver.get('https://www.airnow.gov/?city=Bellevue&state=WA&country=USA')
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    print(soup.prettify())
+    #day_one_div = soup.find(id='day-1')
+    #print(day_one_div.prettify())
+    #driver.quit()
+    #print(day_one_div.prettify())
+    #aqi_class = day_one_div.find(class_="day-aqi-value")
+    #print(aqi_class['title'])
+    #nextDay = soup.findall(id='day-1')
+    #print(nextDay)
 
     suggestions = ""
     if maxtemp > 72:
