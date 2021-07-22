d2f5bf920ebc9cac8ae2f98636dd9f1a2c7e00fe
Kevin Arne <kevin.arne@gmail.com>
2021-07-22 14:26:36 -0700

    Changed to using the airnow.gov API to pull the air quality data.

--- run.py
+++ run.py
@@ -2,17 +2,14 @@ import requests
 from decouple import config
 from plyer import notification
 import smtplib, ssl
-from bs4 import BeautifulSoup
-from selenium import webdriver
-
-driver = webdriver.Chrome()
+import datetime
 
 port = 465
 
 if __name__ == "__main__":
     # Request two days of data (today and tomorrow) from openweather at the location used in environmental variables.
     weather_response = requests.get(
-        'https://api.openweathermap.org/data/2.5/onecall'+
+        "https://api.openweathermap.org/data/2.5/onecall"+
         f"?lat={config('LATITUDE')}" +
         f"&lon={config('LONGITUDE')}" +
         "&units=imperial" +
@@ -22,29 +19,32 @@ if __name__ == "__main__":
     # Access the second day temperatures and the associated maximum
     maxtemp = weather_response.json()['daily'][1]['temp']['max']
 
-    notification.notify(
-        title="Weather Alert",
-        message=f"Tomorrow's max temp is {maxtemp}F",
-        timeout=10
-    )
-
-    driver.get('https://www.airnow.gov/?city=Bellevue&state=WA&country=USA')
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    print(soup.prettify())
-    #day_one_div = soup.find(id='day-1')
-    #print(day_one_div.prettify())
-    #driver.quit()
-    #print(day_one_div.prettify())
-    #aqi_class = day_one_div.find(class_="day-aqi-value")
-    #print(aqi_class['title'])
-    #nextDay = soup.findall(id='day-1')
-    #print(nextDay)
-
     suggestions = ""
     if maxtemp > 72:
-        suggestions += "Make sure to open the windows and run the fans."
+        suggestions += "Make sure to open the windows and run the fans.\n"
+
+    air_response = requests.get(
+        "https://www.airnowapi.org/aq/forecast/zipCode/"+
+        "?format=JSON"+
+        f"&zipCode={config('ZIPCODE')}"+
+        f"&date={(datetime.datetime.now() + datetime.timedelta(days=1)).strftime('%Y-%m-%d')}"+
+        f"&API_KEY={config('AIR_NOW_API')}"
+        )
+
+    air_quality = air_response.json()[0]['Category']['Name']
+
+    if air_quality != "Good":
+        suggestions += "Run air filter and keep windows closed. If ventilation needed, try placing filters over windows.\n"
+
+    notification.notify(
+        title="Weather Alert",
+        message=
+            f"Tomorrow's max temp is {maxtemp}F\n" +
+            f"Expected air quality: {air_quality}",
+        timeout=10
+    )
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(config('EMAIL_USER'), config('EMAIL_PASS'))
-        server.sendmail(config('EMAIL_USER'), config('MAIN_EMAIL'), f"Subject: MaxTemp: {maxtemp}F\n\n{suggestions}")
+        server.sendmail(config('EMAIL_USER'), config('MAIN_EMAIL'), f"Subject: Max Temp: {maxtemp}F Air Quality: {air_quality}\n\n{suggestions}")
