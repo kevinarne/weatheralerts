dc3ea8908723f6d2e8e2b4e81083d1ddd0913590
Kevin Arne <kevin.arne@gmail.com>
2021-07-21 19:49:50 -0700

    Added email notification.

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import requests
 from decouple import config
 from plyer import notification
+import smtplib, ssl
+
+port = 465
 
 if __name__ == "__main__":
     # Request two days of data (today and tomorrow) from openweather at the location used in environmental variables.
@@ -17,5 +20,14 @@ if __name__ == "__main__":
     notification.notify(
         title="Weather Alert",
         message=f"Tomorrow's max temp is {maxtemp}F",
-        timeout=3600
+        timeout=10
     )
+
+    suggestions = ""
+    if maxtemp > 72:
+        suggestions += "Make sure to open the windows and run the fans."
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(config('EMAIL_USER'), config('EMAIL_PASS'))
+        server.sendmail(config('EMAIL_USER'), config('MAIN_EMAIL'), f"Subject: MaxTemp: {maxtemp}F\n\n{suggestions}")
